2f52bf15bf287e1e446ee2a98c873bf54c16ea88
HideakiKawashima <hideshimabaseball0531@ezweb.ne.jp>
2024-06-30 23:52:08 +0900

    Add increment and decrement functionality

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,23 @@ class UTip extends StatefulWidget {
 }
 
 class _UTipState extends State<UTip> {
+  int _personCount = 1;
+
+  //Methods
+  void increment() {
+    setState(() {
+      _personCount = _personCount + 1;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      if (_personCount > 0) {
+        _personCount--;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print(context.widget);
@@ -40,30 +57,82 @@ class _UTipState extends State<UTip> {
     );
     return Scaffold(
       appBar: AppBar(
-        title: Text('UTip'),
+        title: const Text('UTip'),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-              padding: const EdgeInsets.all(8),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+                padding: const EdgeInsets.all(18),
+                decoration: BoxDecoration(
+                    color: theme.colorScheme.inversePrimary,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  children: [
+                    Text(
+                      "Total per Person",
+                      style: style,
+                    ),
+                    Text(
+                      "\$23.89",
+                      style: style.copyWith(
+                          color: theme.colorScheme.onPrimary,
+                          fontSize: theme.textTheme.displaySmall?.fontSize),
+                    ),
+                  ],
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
               decoration: BoxDecoration(
-                  color: theme.colorScheme.inversePrimary,
-                  borderRadius: BorderRadius.circular(10)),
+                  borderRadius: BorderRadius.circular(5),
+                  border:
+                      Border.all(color: theme.colorScheme.primary, width: 2)),
               child: Column(
                 children: [
-                  Text(
-                    "Total per Person",
-                    style: style,
+                  TextField(
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.attach_money),
+                        labelText: 'Bill Amount'),
+                    keyboardType: TextInputType.number,
+                    onChanged: (String value) {
+                      print("Value: $value ");
+                    },
                   ),
-                  Text(
-                    "\$23.89",
-                    style: style.copyWith(
-                        color: theme.colorScheme.onPrimary,
-                        fontSize: theme.textTheme.displaySmall?.fontSize),
-                  ),
+                  //Split Bill area
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Split',
+                        style: theme.textTheme.titleMedium,
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                              color: theme.colorScheme.primary,
+                              onPressed: decrement,
+                              icon: const Icon(Icons.remove)),
+                          Text(
+                            "$_personCount",
+                            style: theme.textTheme.titleMedium,
+                          ),
+                          IconButton(
+                              color: theme.colorScheme.primary,
+                              onPressed: increment,
+                              icon: const Icon(Icons.add)),
+                        ],
+                      )
+                    ],
+                  )
                 ],
-              )),
+              ),
+            ),
+          )
         ],
       ),
     );
